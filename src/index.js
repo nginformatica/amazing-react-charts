edd4579fd051c199f7753a369edec1d80f7da664
leonardo.rigon <leonardo.rigon@ngi.com.br>
2019-12-04 19:17:42 -0300

    docz configured at project, remove unecessary files, added paths o the gitignore

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { KeepfyReactCharts } from './KeepfyReactCharts'
-import * as serviceWorker from './serviceWorker'
-
-ReactDOM.render(<KeepfyReactCharts />, document.getElementById('root'))
-
-serviceWorker.unregister()
