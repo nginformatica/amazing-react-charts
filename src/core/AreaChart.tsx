6b2819b7c6dad327b6325e21e4377609620f32d0
leonardo.rigon <leonardo.rigon@ngi.com.br>
2019-12-05 12:06:10 -0300

    area chart component added and finished, dependencies added

--- src/core/AreaChart.tsx
+++ src/core/AreaChart.tsx
@@ -2,81 +2,209 @@ import * as React from 'react'
 import ReactEcharts from 'echarts-for-react'
 import { format, parse } from 'date-fns'
 import ptBR from 'date-fns/locale/pt-BR'
+import { takeLast } from 'ramda'
 
 interface IProps {
     data: TData[]
+    tooltip?: TTooltip
     color?: string
     xType?: 'time' | 'category'
+    yType?: 'time' | 'value'
 }
+
+type TTooltip = { label: string, result: string }
 
 type TData = { label: string, result: number }
 
 type TSeries = {
-    name?: string,
-    yAxisIndex?: number,
-    type: 'line' | 'bar' 
-    data: number[] | string[] | Date[]
+    name?: string
+    yAxisIndex?: number
+    label?: TLabelProps
     areaStyle?: React.CSSProperties
     lineStyle?: React.CSSProperties
+    type: 'line' | 'bar'
+    data: number[] | string[] | Date[]
+}
+
+type TLabelProps = {
+    normal?: TNormalProps
+}
+
+type TNormalProps = {
+    show: boolean,
+    position?: 'top' | 'bottom' | 'left' | 'right' | [number, number]
+    fontSize?: number
+    color?: string
+    distance?: number
+}
+
+type TGridProps = {
+    show: boolean
 }
 
 type TAxisLabel = {
+    // TODO: Type formatter correctly
     formatter?: any
     textStyle?: React.CSSProperties
     interval?: number | 'auto'
+    rotate?: number
 }
 
-type TAxisProps = { 
+type TSplitLineProps = {
+    show?: boolean
+    lineStyle?: {
+        color?: string | string[]
+        width?: number
+        opacity?: number
+        type?: 'solid' | 'dashed' | 'dotted'
+    }
+}
+
+type TAxisProps = {
+    name?: string
     type?: 'category' | 'value' | 'time'
     boundaryGap?: boolean
     data?: number[] | string[] | Date[]
     axisLabel?: TAxisLabel
+    gridIndex?: number
+    showGrid?: boolean
+    splitLine?: TSplitLineProps
 }
 
 interface IOptions {
     series: TSeries[]
     xAxis: TAxisProps
     yAxis: TAxisProps
+    grid?: TGridProps
 }
+
+type TTooltipProps = {
+    // TODO: Type formatter correctly
+    formatter: any
+    trigger?: 'axis'
+    textStyle?: React.CSSProperties
+}
+
 
 export const toDate = (text: string) => parse(text, 'yyyy-MM-dd', new Date())
 
+export const formatLabel = (text: string) =>
+    format(new Date(text), 'dd MMM', { locale: ptBR })
+
+export const formatTooltip = (text: string) =>
+    format(new Date(text), 'dd/MM/yyyy')
+
+export const timeConvert = (value: number) => {
+    const seconds = Math.round((value % 1) * 3600)
+    const minutes = Math.trunc(seconds / 60)
+    const formatedMinutes = takeLast(2, '0' + minutes)
+
+    return minutes > 0
+        ? Math.round(value) + ':' + formatedMinutes
+        : Math.round(value) + ':00'
+}
+
 const AreaChart = (props: IProps) => {
-    const { data, xType, color } = props
+    const { data, xType, color, tooltip: tooltipProps, yType } = props
 
     const yData = props.data.map(item => item.result)
-    const xData = xType === 'time' 
-        ? data.map(item => format(toDate(item.label), 'dd MMM', { locale: ptBR }))
+    const xData = xType === 'time'
+        ? data.map(item => toDate(item.label))
         : data.map(item => item.label)
+
+    //TODO: Type formatSingleTootltip correctly
+    const formatSingleTooltip = (chartValues: any) => {
+        const { label, result } = tooltipProps
+        const { axisValueLabel, data } = chartValues[0]
+        const values = yType === 'time' ? timeConvert(data) + 'h' : data 
+
+        return [
+            `${label}: ${formatTooltip(axisValueLabel)} <br>` +
+            `${result}: ${values} <br>`
+        ]
+    }
+
+
+    const tooltip: TTooltipProps = {
+        formatter: formatSingleTooltip,
+        trigger: 'axis',
+        textStyle: { fontSize: 11.5 }
+    }
+
 
     const options: IOptions = {
         series: [{
             name: 'costs',
             type: 'line',
             data: yData,
+            label: {
+                normal: {
+                    show: true,
+                    position: 'top',
+                    fontSize: 11.5,
+                    color: 'black',
+                    distance: 1.1
+                }
+            },
             lineStyle: {
                 color: color || 'blue'
             },
-            areaStyle: { 
-                color: color || 'blue', 
-                opacity: 0.4
-            } 
+            areaStyle: {
+                color: color || 'blue',
+                opacity: 0.2
+            }
         }],
-        xAxis: { 
+        xAxis: {
             type: 'category',
             boundaryGap: false,
+            showGrid: true,
             data: xData,
+            splitLine: {
+                show: true,
+                lineStyle: {
+                    type: 'dotted',
+                    opacity: 0.8
+                }
+            },
             axisLabel: {
+                formatter:
+                    (item: string) => xType === 'time' ? formatLabel(item) : item,
+                rotate: xData.length >= 24 ? 45 : 0,
                 interval: 0,
-                textStyle: { 
-                    fontSize: 10 
+                textStyle: {
+                    fontSize: 11.5
                 }
             }
         },
-        yAxis: { type: 'value' },
+        yAxis: {
+            type: 'value',
+            splitLine: {
+                show: true,
+                lineStyle: {
+                    type: 'dotted',
+                    opacity: 0.8
+                }
+            },
+            axisLabel: {
+                formatter:
+                    (item: number) => yType === 'time' ? timeConvert(item) + 'h' : item,
+                textStyle: {
+                    fontSize: 11.5
+                }
+            }
+        },
+        grid: { show: true }
     }
 
-    return <ReactEcharts option={ options } />
+    return (
+        <ReactEcharts
+            option={
+                tooltipProps
+                    ? { ...options, tooltip }
+                    : options
+            }
+        />
+    )
 }
 
 export default AreaChart
