430abb24ebf13033c930b26ab56e56560ff2dfa7
leonardo.rigon <leonardo.rigon@ngi.com.br>
2019-12-10 13:58:27 -0300

    remove --emitDeclarationOnly added prebuild

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { default as AreaChart } from './core/AreaChart'
 export { default as HorizontalBarChart } from './core/HorizontalBarChart'
 export { default as StackedBarChart } from './core/StackedBarChart'
+export { default as VerticalBarChart} from './core/VerticalBarChart'
