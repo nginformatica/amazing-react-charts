f6bd93674b061ee9d4ff93e99ce1a149d04aafdb
leorigon <leorigonn@hotmail.com>
2020-04-24 14:55:06 -0300

    fix(tickLine-horizontal): allow the axisLine on vertical bar chart

--- src/core/HorizontalBarChart.tsx
+++ src/core/HorizontalBarChart.tsx
@@ -154,7 +154,7 @@ const HorizontalBarChart = (props: IProps) => {
             data: yData,
             type: 'category',
             axisLine: {
-                show: false
+                show: showTickInfos || false,
             },
             axisLabel: {
                 formatter: (text: string) => truncateLabel(text, labelWordSize),
