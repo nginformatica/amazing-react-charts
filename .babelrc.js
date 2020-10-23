f1a7cbb3777fd66b7f5317593c7b6bf4dbf5b007
leonardo.rigon <leonardo.rigon@ngi.com.br>
2020-10-23 13:37:42 -0300

    fix(babel): remove unecessary transform, downgrade react-dom, change locale import

--- .babelrc.js
+++ .babelrc.js
@@ -1,19 +1,8 @@
-const toKebabCase = (str) =>
-  str.replace(
-    /([a-z])?([A-Z])/g,
-    (m, g1, g2) => (g1 ? g1 + "-" : "") + g2.toLowerCase()
-  );
-
 module.exports = {
   plugins: [
     [
       "transform-imports",
       {
-        "date-fns/locale": {
-          transform: (importName) =>
-            `date-fns/locale/${toKebabCase(importName)}`,
-          preventFullImport: true
-        },
         "date-fns": {
           transform: "date-fns/${member}",
           preventFullImport: true
