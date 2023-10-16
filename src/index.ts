97e14e1070a2e577d50d23ae91f993675b2e31b2
Karine Brandelli Padilha <karine.padilha@ngi.com.br>
2023-10-16 14:29:49 -0300

    chore: adjust exports max length line

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,51 @@
-export { default as AreaChart } from './core/AreaChart'
-export { default as HorizontalBarChart } from './core/HorizontalBarChart'
-export { default as StackedBarChart } from './core/StackedBarChart'
-export { default as VerticalBarChart } from './core/VerticalBarChart'
-export { default as PieChart } from './core/PieChart'
-export { default as LineChart } from './core/LineChart'
-export { default as ForecastAreaChart } from './core/ForecastAreaChart'
-export { default as PictorialChart } from './core/PictorialChart'
-export { default as AudiometryChart } from './core/AudiometryChart'
-export { default as DonutChart } from './core/DonutChart'
-export { default as CoordinateLineChart } from './core/CoordinateLineChart'
-export { default as MultipurposeChart } from './core/MultipurposeChart'
-export { default as RadarChart } from './core/RadarChart'
+export {
+  default as AreaChart
+} from './core/area-chart/AreaChart'
+
+export {
+  default as AudiometryChart
+} from './core/audiometry-chart/AudiometryChart'
+
+export {
+  default as CoordinateLineChart
+} from './core/coordinate-line-chart/CoordinateLineChart'
+
+export {
+  default as DonutChart
+} from './core/donut-chart/DonutChart'
+
+export {
+  default as ForecastAreaChart
+} from './core/forecast-area-chart/ForecastAreaChart'
+
+export {
+  default as HorizontalBarChart
+} from './core/horizontal-bar-chart/HorizontalBarChart'
+
+export {
+  default as LineChart
+} from './core/line-chart/LineChart'
+
+export {
+  default as MultipurposeChart
+} from './core/multipurpose-chart/MultipurposeChart'
+
+export {
+  default as PictorialChart
+} from './core/pictorial-chart/PictorialChart'
+
+export {
+  default as PieChart
+} from './core/pie-chart/PieChart'
+
+export {
+  default as RadarChart
+} from './core/radar-chart/RadarChart'
+
+export {
+  default as StackedBarChart
+} from './core/stacked-bar-chart/StackedBarChart'
+
+export {
+  default as VerticalBarChart
+} from './core/vertical-bar-chart/VerticalBarChart'
